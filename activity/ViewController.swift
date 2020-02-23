f2409669d987efa9f4782fd15c1fd0403d0d4dfa
Moskalenko Nikita <n.moskalneko1996@yahoo.com>
2020-02-23 04:41:47 +0300

    Initial Commit

---
+++ activity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  activity
+//
+//  Created by Nikita Moskalenko on 2/23/20.
+//  Copyright © 2020 Nikita Moskalenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
